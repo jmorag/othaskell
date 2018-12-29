45cd57e4d296f942284355c1ea5e1a2f9be04e43
Joseph Morag <jm4157@columbia.edu>
2018-12-29 16:34:33 +0200

    Formatting in main

--- app/Main.hs
+++ app/Main.hs
@@ -9,25 +9,25 @@ import Graphics.Gloss.Interface.IO.Simulate
 
 
 main :: IO ()
-main = simulateIO
-  (InWindow "game" (800, 800) (0, 0))
-  (dark $ dark green)
-  1
-  hardGame
-  (return . renderGloss)
-  (\_ _ gs -> do
-    next <- runReaderT (stepGame gs)
-                       (glossConfig negamaxStrategy trivialStrategy)
-    case next of
-      Right gs'      -> return gs'
-      Left  Finished -> do
-        putText
-          $  "Game over: Black :: "
-          <> show (fst (score gs))
-          <> ", White :: "
-          <> show (snd (score gs))
-        return gs
-      Left Timeout -> do
-        putText $ "Player " <> show (_player gs) <> " timed out"
-        return gs
-  )
+main =
+  simulateIO (InWindow "game" (800, 800) (0, 0))
+             (dark . dark $ green)
+             1
+             easyGame
+             (return . renderGloss)
+    $ \_ _ gs -> do
+        next <- runReaderT (stepGame gs)
+                           (Config negamaxStrategy trivialStrategy)
+        case next of
+          Right gs'      -> return gs'
+          Left  Finished -> do
+            putText
+              $  "Game over: Black :: "
+              <> show (fst (score gs))
+              <> ", White :: "
+              <> show (snd (score gs))
+            exitSuccess
+          Left Timeout -> do
+            putText $ "Player " <> show (_player gs) <> " timed out"
+            exitSuccess
+
